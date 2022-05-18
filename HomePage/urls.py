a5fc7c7c5e86f3de141f0cff612e57b7a0ee517c
unknown <shaunclfedrick@gmail.com>
2022-05-18 14:13:44 -0400

    tabs become active when you click on them now and all tabs are linked

--- HomePage/urls.py
+++ HomePage/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns=[
     path('',views.home),
+    path('updates',views.updates),
+    path('aboutme',views.aboutme),
     path('portfolio',views.portfolio),
     path('art',views.art),
     path('resume',views.resume)
--- HomePage/views.py
+++ HomePage/views.py
@@ -4,17 +4,24 @@ from django.template.loader import render_to_string
 # Create your views here.
 
 def home(request):
-    home_var=render_to_string("aboutme.html")
-    return render(request,"headerfooter.html",{'content':home_var})
-
+    #updates page is the home page
+    home_var=render_to_string("homepage.html")
+    return render(request,"headerfooter.html",{'content':home_var,'home_active':True})
+def updates(request):
+    #updates page is the home page
+    updates_var=render_to_string("homepage.html")
+    return render(request,"headerfooter.html",{'content':updates_var,'updates_active':True})
+def aboutme(request):
+    about_var=render_to_string("aboutme.html")
+    return render(request,"headerfooter.html",{'content':about_var,'aboutme_active':True})
 def portfolio(request):
     portfolio_var=render_to_string("portfolio.html")
-    return render(request,"headerfooter.html",{'content':portfolio_var})
+    return render(request,"headerfooter.html",{'content':portfolio_var,'portfolio_active':True})
 
 def art(request):
     art_var=render_to_string("art.html")
-    return render(request,"headerfooter.html",{'content':art_var})
+    return render(request,"headerfooter.html",{'content':art_var,'art_active':True})
 
 def resume(request):
     resume_var=render_to_string("resume.html")
-    return render(request,"headerfooter.html",{'content':resume_var})
+    return render(request,"headerfooter.html",{'content':resume_var,'resume_active':True})
