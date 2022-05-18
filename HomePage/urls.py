a0c03b33aa66ac91f868502db0913f6b08c618f9
unknown <shaunclfedrick@gmail.com>
2022-05-18 11:57:57 -0400

    add html pages to views and urls

--- HomePage/urls.py
+++ HomePage/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views 
+)
 
 urlpatterns=[
-    path('',views.home)
+    path('',views.home),
+    path('\portfolio',views.portfolio),
+    path('\art',views.art),
+    path('\resume',views.resume),
+
 ]
--- HomePage/views.py
+++ HomePage/views.py
@@ -4,5 +4,17 @@ from django.template.loader import render_to_string
 # Create your views here.
 
 def home(request):
-    home=render_to_string("aboutme.html")
-    return render(request,"headerfooter.html",{'content':home})
+    home_var=render_to_string("aboutme.html")
+    return render(request,"headerfooter.html",{'content':home_var})
+
+def portfolio(request):
+    portfolio_var=render_to_string("portfolio.html")
+    return render(request,"headerfooter.html",{'content':portfolio_var})
+
+def art(request):
+    art_var=render_to_string("art.html")
+    return render(request,"headerfooter.html",{'content':art_var})
+
+def resume(request):
+    resume_var=render_to_string("resume.html")
+    return render(request,"headerfooter.html",{'content':resume_var})
