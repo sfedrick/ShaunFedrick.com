d7e05632143e89d92b4d01f1ba5fb880c95586a7
sfedrick <sfedrick@haverford.edu>
2022-06-03 18:21:11 -0400

    added a case that check if you're in production or local at the beginning of settings.py

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+Production=True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -121,12 +121,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# in development turn off stat root 
-#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'staticfiles'),
-]
+# in local turn off static_root
+if(Production):
+    STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR,'static'),
+    ]
+else:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR,'staticfiles'),
+    ]
+
 
 
 
