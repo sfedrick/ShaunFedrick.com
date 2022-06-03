c386458f09e9d5686386b80c41fb422197cc8b79
sfedrick <sfedrick@haverford.edu>
2022-06-03 18:10:38 -0400

    get static files working on local machine

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -28,6 +28,7 @@ DEBUG = str(os.environ.get('DebugKey')).replace('\'','')=="True"
 
 
 
+
 ALLOWED_HOSTS = ['127.0.0.1','shaunfedrick.herokuapp.com','www.shaunfedrick.com']
 
 
@@ -120,10 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+# in development turn off stat root 
+#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'staticfiles'),
 ]
 
 
