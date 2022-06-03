cba90d6781c048a4db67f6cc045ea76d7efeed96
sfedrick <sfedrick@haverford.edu>
2022-06-03 18:25:00 -0400

    added fix to work on production site locally

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# change to match whether you are working in production or locally
 Production=True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
